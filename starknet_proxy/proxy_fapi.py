93f4aea243895af68cef9eafcd7a20c2c6318841
Lancelot de Ferrière <wraitii@gmail.com>
2021-11-03 16:19:08 +0100

    Further work on fastAPI

--- starknet_proxy/proxy_fapi.py
+++ starknet_proxy/proxy_fapi.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException
 from fastapi.responses import Response, RedirectResponse
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -32,10 +32,13 @@ import json
 briq_contract = ContractWrapper(abi_json=json.load(open("briq_abi.json", "r")), address=ADDRESS)
 set_contract = ContractWrapper(abi_json=json.load(open("set_abi.json", "r")), address=SET_ADDRESS)
 
+from .storage.storage import get_storage
+storage_client = get_storage()
+print(storage_client)
+
 @app.post("/get_bricks/{owner}")
 @app.get("/get_bricks/{owner}")
 async def get_bricks(owner: int):
-    print("get_bricks called")
     tokens = briq_contract.tokens_at_index(owner=owner, index=0).call()
     balance = await briq_contract.balance_of(owner=owner).call()
     assert isinstance(balance, int)
@@ -49,10 +52,41 @@ async def get_bricks(owner: int):
     parsed_ret = []
     for i in range(min(balance, len(ret)//3)):
         parsed_ret.append((hex(int(ret[i*3])), int(ret[i*3+1]), int(ret[i*3+2])))
+
     return {
         "code": 200,
         "value": parsed_ret
     }
+
+
+@app.post("/store_get/{token_id}")
+@app.get("/store_get/{token_id}")
+async def store_get(token_id: int):
+    owner = await set_contract.owner_of(token_id).call()
+    if owner == 0:
+        HTTPException(status_code=500, detail="Set does not exist")
+
+    try:
+        data = storage_client.load_json(path=str(token_id))
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=500, detail="File not found")
+
+    return {
+        "code": 200,
+        "owner": owner,
+        "token_id": token_id,
+        "data": data
+    }
+
+@app.post("/store_list")
+@app.get("/store_list")
+def store_list():
+    return {
+        "code": 200,
+        "sets": storage_client.list_json()
+    }
+
 
 # Proxy other get/post requests
 @app.post("/{path_name:path}")
