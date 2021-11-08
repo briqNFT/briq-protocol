51f3fed53569d7e574ad080160fc78742cf5f215
Lancelot de Ferrière <wraitii@gmail>
2021-11-08 15:01:28 +0100

    Minor linting update with flake8

--- starknet_proxy/contract.py
+++ starknet_proxy/contract.py
@@ -1,10 +1,9 @@
-import json
 from typing import Any, Union
-import pytest
 
+from starkware.crypto.signature.signature import pedersen_hash, sign
 from starkware.starknet.testing.contract import StarknetContract
 from starkware.starknet.compiler.compile import get_selector_from_name
-from starkware.starknet.services.api.gateway.transaction import Deploy, InvokeFunction
+from starkware.starknet.services.api.gateway.transaction import InvokeFunction
 from services.external_api.base_client import RetryConfig
 from starkware.starknet.services.api.feeder_gateway.feeder_gateway_client import FeederGatewayClient
 from starkware.starknet.services.api.gateway.gateway_client import GatewayClient
@@ -16,6 +15,7 @@ retry_config = RetryConfig(n_retries=1)
 client = GatewayClient(url="https://alpha3.starknet.io/gateway/", retry_config=retry_config)
 feeder_client = FeederGatewayClient(url="https://alpha3.starknet.io/feeder_gateway/", retry_config=retry_config)
 
+
 def set_gateway(url: str):
     global client
     global feeder_client
@@ -23,17 +23,18 @@ def set_gateway(url: str):
     client = GatewayClient(url=url + "gateway/", retry_config=retry_config)
     feeder_client = FeederGatewayClient(url=url + "feeder_gateway/", retry_config=retry_config)
 
+
 def felt_formatter(hex_felt: str) -> Union[str, int]:
-    #return field_element_repr(val=int(hex_felt, 16), prime=fields.FeltField.upper_bound)
     try:
         return int(hex_felt, 16)
-    except:
+    except Exception:
         return field_element_repr(val=int(hex_felt, 16), prime=fields.FeltField.upper_bound)
+
 
 class FuncInvocation:
     def __init__(self, tx):
         self.tx = tx
-    
+
     async def call(self) -> Union[Union[str, int], list[Union[str, int]]]:
         res = await feeder_client.call_contract(self.tx, None)
         res = list(map(felt_formatter, res['result']))
@@ -44,6 +45,7 @@ class FuncInvocation:
     async def invoke(self):
         print(f"invoking with {self.tx}")
         return await client.add_transaction(self.tx)
+
 
 class ContractWrapper:
     def __init__(self, abi_json: list[Any], address: str) -> None:
@@ -56,6 +58,7 @@ class ContractWrapper:
             getattr(self.contract, name)
         except:
             raise
+
         def method(*args, **kwargs):
             cd = getattr(self.contract, name)(*args, **kwargs).calldata
             selector = get_selector_from_name(name)
@@ -68,9 +71,6 @@ class ContractWrapper:
             return FuncInvocation(tx)
         return method
 
-from starkware.crypto.signature.signature import (
-    pedersen_hash, sign
-)
 
 class ProxyFuncInvocation:
     def __init__(self, contract: Any, sender: int, to: int, selector: int, calldata: list[int]) -> None:
@@ -87,8 +87,8 @@ class ProxyFuncInvocation:
         if len(self.cd) == 1:
             cd_hash = self.cd[0]
         else:
-            cd_hash = pedersen_hash(self.cd[len(self.cd)-1], self.cd[len(self.cd)-2])
-            for i in range(len(self.cd)-2, 0, -1):
+            cd_hash = pedersen_hash(self.cd[len(self.cd) - 1], self.cd[len(self.cd) - 2])
+            for i in range(len(self.cd) - 2, 0, -1):
                 cd_hash = pedersen_hash(cd_hash, self.cd[i])
         hash = pedersen_hash(hash, cd_hash)
         nonce = await self.contract.contract.get_nonce().call()
@@ -106,13 +106,14 @@ class ProxyFuncInvocation:
     #async def invoke(self):
     #    return await FuncInvocation.invoke(self)
 
+
 class AccountProxy:
     def __init__(self, abi_json: list[Any], address: str, proxy_abi: list[Any], proxy_address: str, private_key: int):
         self.private_key = private_key
 
         self.contract = ContractWrapper(abi_json, address)
         self.address = int(address, 16)
-        
+
         self.target = StarknetContract(state=None,
             abi=proxy_abi,
             contract_address=proxy_address)
