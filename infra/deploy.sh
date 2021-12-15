042331e57fe66eec5792b2e3d9c42029c16e5677
Lancelot de Ferrière <wraitii@gmail.com>
2021-12-15 18:50:07 +0100

    Split k8s templates for prod & dev branches
    Cleanup by not including anything twice.

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -2,6 +2,8 @@
 set -e
 
 export TAG=${TAG:=$(git rev-parse HEAD)}
+export K8S_ENV=${K8S_ENV:="test"}
+
 infra/run-docker.sh
 infra/render.sh
-kubectl apply -f infra/dist/manifests.yaml --validate=true
+kubectl apply -f infra/dist/manifests-${K8S_ENV}.yaml --validate=true
--- infra/render.sh
+++ infra/render.sh
@@ -3,9 +3,11 @@ set -e
 
 TAG=${TAG:=$(git rev-parse HEAD)}
 
+K8S_ENV=${K8S_ENV:="test"}
+
 mkdir -p infra/dist
-ytt -f infra/schema.yaml -f infra/ --data-value "image_tag=$TAG" > infra/dist/manifests.yaml
+ytt -f infra/schema.yaml -f infra/vals-${K8S_ENV}.yaml -f infra/k8s/ --data-value "image_tag=$TAG" > infra/dist/manifests-${K8S_ENV}.yaml
 echo "validating client-side"
-kubectl apply -f infra/dist/manifests.yaml --validate=true --dry-run=client
+kubectl apply -f infra/dist/manifests-${K8S_ENV}.yaml --validate=true --dry-run=client
 echo "validating server-side"
-kubectl apply -f infra/dist/manifests.yaml --validate=true --dry-run=server
+kubectl apply -f infra/dist/manifests-${K8S_ENV}.yaml --validate=true --dry-run=server
