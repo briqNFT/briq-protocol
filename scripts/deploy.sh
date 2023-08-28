dcd72e6e0d2bba2582136c6688f92b6f22ae22d3
Lancelot de Ferrière <wraitii@gmail.com>
2023-08-28 11:04:38 +0200

    Rework scripts folder, WIP work for testnet

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,21 +1,12 @@
-# Run katana
-# Configure for starkli
-export STARKNET_RPC="http://localhost:5050/"
-
-# password is katana
-export STARKNET_KEYSTORE="scripts/katana_signer"
-export KEYSTORE_PWD="katana"
-
-export STARKNET_ACCOUNT="scripts/katana_account.json"
-export ACCOUNT_ADDRESS=$(jq .deployment.address scripts/katana_account.json -r)
-export TREASURY_ADDRESS="0x33c627a3e5213790e246a917770ce23d7e562baa5b4d2917c23b1be6d91961c"
-
-# https://github.com/dojoengine/dojo/blob/main/crates/katana/core/src/constants.rs
-# katana predeployed fee_token_address (uses transferFrom..)
-export FEE_TOKEN_ADDR="0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7"
+# Source one of the setup scripts.
 
 sozo build
 
+sozo migrate
+
+export WORLD_ADDRESS=0x598f57e84be4279ff74e7ba389be4d46ec2b1c7974088caad07ebd5a3e8baaa
+export EXECUTOR_ADDRESS=0x75f86ab304522ccf46e65201024f9abbe708ac76639bece351921fae1831f7d
+  
 echo "\nDeclaring class hashes ...\n"
 
 export BRIQ_HASH=$(starkli declare --compiler-version 2.1.0 target/dev/briq_protocol-BriqToken.json --keystore-password $KEYSTORE_PWD)
@@ -27,12 +18,6 @@ echo BRIQ_HASH=$BRIQ_HASH
 echo SET_HASH=$SET_HASH
 echo ERC1155_HASH=$ERC1155_HASH
 echo "*************************************"
-
-sozo migrate
-
-export WORLD_ADDRESS=0x23aa0e3ffa4663cca36a00577eb9c188aff648568c859735e6de463d41713ec
-export EXECUTOR_ADDRESS=0x461be0e8caa002e9fa011760f4412fdad3579afc58cc120f22c794a888bfb6b
-
 
 echo "\nDeploying contracts ...\n"
 
